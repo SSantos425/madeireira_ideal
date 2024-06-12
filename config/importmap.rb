37de1de6f90a585f9fe91003b1932cd17949d7fe
SSantos425 <samuel.tri@hotmail.com>
2024-06-12 17:04:54 -0300

    fix 5 for js

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,6 +6,6 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 
-pin "jquery", to: "vendor/assets/plugins/jquery/jquery.min.js"
-pin "bootstrap", to: "vendor/assets/plugins/bootstrap/js/bootstrap.bundle.min.js"
-pin "adminlte", to: "vendor/assets/dist/js/adminlte.min.js"
+pin "jquery", to: "/assets/plugins/jquery/jquery.min.js"
+pin "bootstrap", to: "/assets/plugins/bootstrap/js/bootstrap.bundle.min.js"
+pin "adminlte", to: "/assets/dist/js/adminlte.min.js"

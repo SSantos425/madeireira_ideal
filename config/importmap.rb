60bd3d768074e1177e1bf089bcdeb2813db14ae7
SSantos425 <samuel.tri@hotmail.com>
2024-06-14 15:16:12 -0300

    change bootstrap and jquery in importmap for cdn

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,6 +6,6 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 
-pin "jquery", to: "/assets/plugins/jquery/jquery.min.js"
-pin "bootstrap", to: "/assets/plugins/bootstrap/js/bootstrap.bundle.min.js"
+pin "jquery", to: "https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"
+pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
 pin "adminlte", to: "/assets/dist/js/adminlte.min.js"

b361447acf92bad10585a624cb7c7bcd193e56bc
SSantos425 <samuel.tri@hotmail.com>
2024-08-26 15:11:08 -0300

    fix dropdown

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -3,3 +3,7 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
+//= link adminlte/plugins/jquery/jquery.min.js
+//= link adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js
+//= link adminlte/dist/js/adminlte.min.js
+

e1e7ba445924b0c71aed5e4ac29bfa421e4fbf77
SSantos425 <samuel.tri@hotmail.com>
2024-08-26 15:53:35 -0300

    fixed manifest

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -3,8 +3,5 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
-//= link adminlte/plugins/jquery/jquery.min.js
-//= link adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js
-//= link adminlte/dist/js/adminlte.min.js
 
 

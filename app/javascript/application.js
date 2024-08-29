52889c67dc356d38109097bcf43f1bf543d76adb
SSantos425 <samuel.tri@hotmail.com>
2024-08-29 12:18:36 -0300

    fix dropdown not working

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,17 +2,10 @@
 
 import "@hotwired/turbo-rails"
 import "controllers"
-import 'jquery'
-import 'bootstrap'
-import 'adminlte'
-// import "./adminlte/plugins/jquery/jquery.min.js"
-// import "./adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"
+import "./adminlte/plugins/jquery/jquery.min.js"
+import "./adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"
 //import "./adminlte/plugins/sweetalert2/sweetalert2.all.min.js"
 //import "./adminlte/plugins/toastr/toastr.min.js"
 import Swal from 'sweetalert2';
 window.Swal = Swal;
 import "./adminlte/dist/js/adminlte.min.js"
-
-
-
-
--- config/importmap.rb
+++ config/importmap.rb
@@ -6,7 +6,3 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "sweetalert2" # @11.12.2
-
-pin "jquery", to: "https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"
-pin "bootstrap", to: "https://cdnjs.cloudflare.com/ajax/libs/admin-lte/3.2.0/js/adminlte.min.js"
-pin "adminlte", to: "https://cdnjs.cloudflare.com/ajax/libs/admin-lte/3.2.0/js/adminlte.min.js"

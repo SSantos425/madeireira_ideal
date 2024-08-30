f865c9fb6a777cebb67e3af602115fc2e6c5efcc
SSantos425 <samuel.tri@hotmail.com>
2024-08-30 13:38:36 -0300

    try fix manifest

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -3,3 +3,6 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
+//= link_tree ../../../vendor/assets/adminlte/plugins/jquery .js
+//= link_tree ../../../vendor/assets/adminlte/plugins/bootstrap/js .js
+//= link_tree ../../../vendor/assets/adminlte/dist/js .js

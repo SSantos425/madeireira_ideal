5408511f69534a4a7d437e9630de8c320f3fb533
SSantos425 <samuel.tri@hotmail.com>
2024-12-10 15:20:09 -0300

    try using cdn to js

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,4 +5,12 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "sweetalert2" # @11.12.2
+# pin "sweetalert2" # @11.12.2
+
+# config/importmap.rb
+
+# Importando as bibliotecas de CDN
+pin "jquery", to: "https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"
+pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"
+pin "adminlte", to: "https://cdn.jsdelivr.net/npm/admin-lte@3.2.0/dist/js/adminlte.min.js"
+pin "sweetalert2", to: "https://cdn.jsdelivr.net/npm/sweetalert2@11.4.25/dist/sweetalert2.all.min.js"
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,14 +5,9 @@ Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets')
-
-# Add paths for AdminLTE assets
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'adminlte', 'dist', 'css')
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'adminlte', 'dist', 'js')
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'adminlte', 'plugins', 'bootstrap', 'js')
-
+Rails.application.config.assets.paths << Rails.root.join('vendor','assets')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w[adminlte/dist/css/*.css adminlte/dist/js/*.js adminlte/plugins/bootstrap/js/*.js]
+# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w[adminlte/dist/css/*.css  adminlte/plugins/jquery/jquery.js]
